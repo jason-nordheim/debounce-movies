0c55ee0bbdd2f227d65ce53eb2d03c1b4eace455
Jason Nordheim <jason.nordheim@gmail.com>
2020-11-05 12:13:40 -0700

    added Movies component to root App component inside main tag

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,21 @@ import './App.css'
 
 // components 
 import Search from './Search'
+import Movies from './Movies'
 
+/**
+ * Root Application component 
+ */
 const App = ()  => {
   const [search, setSearch] = useState('')
   
   return (
     <div className="app">
       <header>Debounce Movies</header>
-      <Search value={search} setValue={setSearch}/> 
+      <main>
+        <Search value={search} setValue={setSearch}/> 
+        <Movies search={search} /> 
+      </main>
     </div>
   );
 }
