3d7c93a260d27f7c980888f09f74e8f64c3fd7a1
Jason Nordheim <jason.nordheim@gmail.com>
2020-11-05 12:47:09 -0700

    added handler and tested getting all movies matching specified type

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,40 @@
-// imports 
-import React, { useState } from 'react' 
+// imports
+import React, { useState } from "react";
+import { searchAny } from "../util/omniMovieApi";
 
-// styles 
-import './App.css'
+// styles
+import "./App.css";
 
-// components 
-import Search from './Search'
-import Movies from './Movies'
+// components
+import Search from "./Search";
+import Movies from "./Movies";
 
 /**
- * Root Application component 
+ * Root Application component
  */
-const App = ()  => {
-  const [search, setSearch] = useState('')
-  
+const App = () => {
+  const [search, setSearch] = useState("");
+
+  /**
+   * Event handler for clicking search
+   * @param {event} e
+   */
+  const handleSearch = async (e) => {
+    e.preventDefault(); // no refresh
+
+    const searchResults = await searchAny(search);
+    console.log(searchResults);
+  };
+
   return (
     <div className="app">
       <header>Debounce Movies</header>
       <main>
-        <Search value={search} setValue={setSearch}/> 
-        <Movies search={search} /> 
+        <Search value={search} setValue={setSearch} onSearch={handleSearch} />
+        <Movies search={search} />
       </main>
     </div>
   );
-}
+};
 
 export default App;
