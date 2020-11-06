b0ee29347613a7243f16eb60dd11b361f51ff0f1
Jason Nordheim <jason.nordheim@gmail.com>
2020-11-06 11:47:48 -0700

    changed header text and added emojis 🤗

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <header>Debounce Movies</header>
+      <header>🎥 Movie Finder 🍿</header>
       <main>
         <Search value={search} setValue={setSearch} onSearch={handleSearch} />
         <Movies searchResults={results} />
